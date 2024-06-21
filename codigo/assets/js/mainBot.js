6b0af7a7e07d573b1227985b487e4c7e52a75096
guisilvas <guilherme7soaress@gmail.com>
2024-06-20 23:30:57 -0300

    Add Sprint 1 and Sprint 2 content

---
+++ codigo/assets/js/mainBot.js
@@ -0,0 +1,82 @@
+function initializeChatbot() {
+    addMessage('bot', 'Olá, eu sou o GuardBot. Irei te ajudar a manter-se mais seguro(a) na internet!\nSelecione uma das opções abaixo:\n1. Ataque DDoS\n\n2. Injeção SQL\n3. Spam\n4. Phishing\n5. Engenharia Social');
+}
+
+function handleSendButtonClick() {
+    const userInput = document.getElementById('user-input').value;
+    if (isValidInput(userInput)) {
+        addMessage('user', userInput);
+        document.getElementById('user-input').value = '';
+        getResponse(userInput);
+    } else {
+        alert('Por favor, digite um número entre 1 e 5.');
+        document.getElementById('user-input').value = '';
+    }
+}
+
+function isValidInput(input) {
+    const validNumbers = ['1', '2', '3', '4', '5'];
+    return validNumbers.includes(input);
+}
+
+function addMessage(sender, text, imageUrl = null, sourceUrl = null) {
+    const chatBox = document.getElementById('chat-box');
+    const message = document.createElement('div');
+    message.className = `message ${sender}`;
+
+    const img = document.createElement('img');
+    img.src = sender === 'bot' ? '../assets/images/bot.png' : '../assets/images/user.png';
+    img.alt = sender === 'bot' ? 'Bot' : 'Usuário';
+
+    const messageContent = document.createElement('div');
+    messageContent.className = 'message-content';
+    messageContent.textContent = text;
+
+    message.appendChild(img);
+    message.appendChild(messageContent);
+    chatBox.appendChild(message);
+
+    if (sourceUrl) {
+        const linkMessage = document.createElement('div');
+        linkMessage.className = `message ${sender}`;
+        
+        const link = document.createElement('a');
+        link.href = sourceUrl;
+        link.textContent = 'Fonte';
+        link.target = '_blank';
+        
+        linkMessage.appendChild(img.cloneNode()); 
+        linkMessage.appendChild(link);
+
+        chatBox.appendChild(linkMessage);
+    }
+
+    if (imageUrl) {
+        const imageMessage = document.createElement('div');
+        imageMessage.className = `message ${sender} message-image`;
+        
+        const image = document.createElement('img');
+        image.src = imageUrl;
+        image.alt = 'Imagem da resposta do bot';
+        
+        imageMessage.appendChild(img.cloneNode());
+        imageMessage.appendChild(image);
+
+        chatBox.appendChild(imageMessage);
+    }
+
+    chatBox.scrollTop = chatBox.scrollHeight;
+}
+
+async function getResponse(userInput) {
+    const response = await fetch('../assets/js/responses.json');
+    const data = await response.json();
+    const reply = data.responses[userInput];
+    const replyText = reply.text || 'Desculpe, não entendi.';
+    const replyImage = reply.image || null;
+    const replySource = reply.source || null;
+
+    setTimeout(() => {
+        addMessage('bot', replyText, replyImage, replySource);
+    }, 500);
+}
